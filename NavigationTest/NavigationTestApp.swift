3e45c1608974ad0be5ee594e3e56f769630d5113
Alejandra SM <alejandrasanmartin2911@gmail.com>
2023-09-27 12:39:23 -0500

    Initial Commit

---
+++ NavigationTest/NavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestApp.swift
+//  NavigationTest
+//
+//  Created by Alejandra San Martin on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
